33a56d26ce16e6da46a9d713ae536f6b35c3c770
Kenmakhanu <73675815+Kenmakhanu@users.noreply.github.com>
2022-05-02 13:51:17 -0400

    Update c1-versions.tf

--- terraform/c1-versions.tf
+++ terraform/c1-versions.tf
@@ -15,7 +15,4 @@ provider "aws" {
   region = "${var.environment == "prod" ? var.aws_region : var.aws_region1}"
   profile = "default"
 }
-/*
-Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal
-$HOME/.aws/credentials
-*/
+
