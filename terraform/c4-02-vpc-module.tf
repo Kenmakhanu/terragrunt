3905ec6d6ef643a1e8d28dab33d9b5bc60e7a23b
Kenmakhanu <73675815+Kenmakhanu@users.noreply.github.com>
2022-05-04 11:24:32 -0400

    Update c4-02-vpc-module.tf

--- terraform/c4-02-vpc-module.tf
+++ terraform/c4-02-vpc-module.tf
@@ -13,15 +13,15 @@ module "vpc" {
   private_subnets = var.vpc_private_subnets  
 
   # Database Subnets
-  database_subnets = var.vpc_database_subnets
-  create_database_subnet_group = var.vpc_create_database_subnet_group
-  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+  #database_subnets = var.vpc_database_subnets
+  #create_database_subnet_group = var.vpc_create_database_subnet_group
+  #create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
   # create_database_internet_gateway_route = true
   # create_database_nat_gateway_route = true
   
   # NAT Gateways - Outbound Communication
-  enable_nat_gateway = var.vpc_enable_nat_gateway 
-  single_nat_gateway = var.vpc_single_nat_gateway
+  #enable_nat_gateway = var.vpc_enable_nat_gateway 
+  #single_nat_gateway = var.vpc_single_nat_gateway
 
   # VPC DNS Parameters
   enable_dns_hostnames = true
@@ -38,7 +38,7 @@ module "vpc" {
   private_subnet_tags = {
     Type = "Private Subnets"
   }  
-  database_subnet_tags = {
-    Type = "Private Database Subnets"
-  }
+  #database_subnet_tags = {
+  #  Type = "Private Database Subnets"
+ # }
 }
